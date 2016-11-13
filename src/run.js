b34544e89283d43265f048b23a6aef3db7ccec06
William Le <wljackson96@gmail.com>
2016-11-12 22:24:22 -0800

    Initliaze express server

---
+++ src/run.js
@@ -0,0 +1,26 @@
+// Import node module library
+// Express. Htttp server manager
+const express = require('express')
+// File system
+const fs = require('fs')
+
+// Const a webport value
+const webport = 4545
+
+// Initliaze express application
+const app = new express()
+app.disable('x-powered-by')
+
+// Setting express port
+app.set('port', webport)
+// Start express server
+app.listen(app.set('port'), function() {
+    console.log(`Server run on ${webport}`)
+})
+
+
+// Default index page
+app.get('/', function(req, res) {
+    let index = fs.readFileSync('./src/template/index.html','utf8')
+    res.end(index)
+})
