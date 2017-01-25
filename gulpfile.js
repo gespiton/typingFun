d74cf537c9c20375eb2f2e300d518991096d1b16
gespiton <1044750905@qq.com>
2017-01-26 00:32:04 +0800

    add header

--- gulpfile.js
+++ gulpfile.js
@@ -21,79 +21,95 @@ const app = new express();
 //       });
 //   });
 
-gulp.task('sass',function(){
-  return gulp.src('asset/sass/main.sass')
-      .pipe(sass({ includePaths: ['asset/sass'], onError: browserSync.notify, }))
-      .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], {cascade: true, }))
-      .pipe(gulp.dest('src/asset/css'))
-      .pipe(browserSync.reload({ stream: true, }));
+gulp.task('sass', function() {
+    return gulp.src('asset/sass/main.sass')
+        .pipe(sass({
+            includePaths: ['asset/sass'],
+            onError: browserSync.notify,
+        }))
+        .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], {
+            cascade: true,
+        }))
+        .pipe(gulp.dest('src/asset/css'))
+        .pipe(browserSync.reload({
+            stream: true,
+        }));
 });
 
-gulp.task('js', function(){
-  return gulp.src('asset/js/**/*.js')
-    .pipe(concat('main.js'))
-    .pipe(minify({
-        ext:{
-            src:'-debug.js',
-            min:'.js'
-        },
-    }))
-    .pipe(gulp.dest('src/asset/js'))
+gulp.task('js', function() {
+    return gulp.src('asset/js/**/*.js')
+        .pipe(concat('main.js'))
+        .pipe(minify({
+            ext: {
+                src: '-debug.js',
+                min: '.js'
+            },
+        }))
+        .pipe(gulp.dest('src/asset/js'))
 });
 
-gulp.task('pug', function(){
+gulp.task('pug', function() {
     return gulp.src('asset/pugs/**/*.pug')
-    .pipe(pug())
-    .pipe(gulp.dest('src/template'))
+        .pipe(pug())
+        .pipe(gulp.dest('src/template'))
 })
 
-gulp.task('server',server());
+gulp.task('server', server());
 
-gulp.task('watch', function(){
-  gulp.watch('asset/sass/**/*.sass',['sass']);
-  gulp.watch('asset/js/**/*.js',['js']);
-  gulp.watch(['asset/pugs/*'], ['pug']);
+gulp.task('watch', function() {
+    gulp.watch('asset/sass/**/*.sass', ['sass']);
+    gulp.watch('asset/js/**/*.js', ['js']);
+    gulp.watch(['asset/pugs/*'], ['pug']);
 
 });
 
-gulp.task('default', ['pug','sass','js','watch','server']);
+gulp.task('default', ['pug', 'sass', 'js', 'watch', 'server']);
 
 
 
 
 
-function server (){
-  app.disable('x-powered-by');
-  // Setting express port
-  app.set('port', webport);
-  // Start express server
-  app.listen(app.set('port'), function() {
-      console.log(`Server run on ${webport}`)
-  });
-  // Default index page
-  app.get('/', function(req, res) {
-      let index = fs.readFileSync('./src/template/index.html','utf8')
-      res.end(index)
-  });
+function server() {
+    app.disable('x-powered-by');
+    // Setting express port
+    app.set('port', webport);
+    // Start express server
 
-  app.get('/typing',function (req,res) {
-      let typing = fs.readFileSync('./src/template/typing.html','utf8')
-      res.end(typing)
-  });
+    // Default index page
+    app.get('/', function(req, res) {
+        let index = fs.readFileSync('./src/template/index.html', 'utf8')
+        res.end(index)
+    });
 
-  app.get('/', function(req, res) {
-      let index = fs.readFileSync('./src/template/index.html','utf8')
-      res.end(index)
-  });
+    app.get('/typing', function(req, res) {
+        let typing = fs.readFileSync('./src/template/typing.html', 'utf8')
+        res.end(typing)
+    });
 
-  app.get('/mainJs',function(req,res){
-      let scriptv = fs.readFileSync('./src/asset/js/main.js','utf8')
-      res.end(scriptv)
-  });
+    app.get('/', function(req, res) {
+        let index = fs.readFileSync('./src/template/index.html', 'utf8')
+        res.end(index)
+    });
 
-  app.get('/main.css', function(req, res) {
-      console.log("style loaded");
-      let style = fs.readFileSync('./src/asset/css/main.css','utf8')
-      res.end(style)
-  });
+    app.get('/mainJs', function(req, res) {
+        let scriptv = fs.readFileSync('./src/asset/js/main.js', 'utf8')
+        res.end(scriptv)
+    });
+
+    app.get('/main.css', function(req, res) {
+        console.log("style loaded");
+        let style = fs.readFileSync('./src/asset/css/main.css', 'utf8')
+        res.end(style)
+    });
+
+// serve static file
+    var path = require('path');
+    console.log(path.toString());
+    var dir = path.join(__dirname, 'src/asset');
+    console.log(dir.toString());
+    app.use(express.static(dir));
+
+    app.listen(app.set('port'), function() {
+        console.log(`Server run on ${webport}`)
+    });
 }
