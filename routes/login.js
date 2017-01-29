d741b760a31c85eef55f4ef10ef3096b36a480be
gespiton <1044750905@qq.com>
2017-01-29 10:34:25 +0800

    fix js load

--- routes/login.js
+++ routes/login.js
@@ -1,9 +1,32 @@
 var express = require('express');
 var router = express.Router();
-
-/* GET home page. */
+var formidable = require("formidable");
+var util = require("util");
+var http = require('http');
 router.post('/', function (req, res, next) {
-    res.end('login logic');
+    console.log('i am in');
+    processAllFieldsOfTheForm(req, res);
 });
 
+function processAllFieldsOfTheForm(req, res) {
+    console.log('i am in process');
+
+    var form = new formidable.IncomingForm();
+
+    form.parse(req, function (err, fields, files) {
+        //Store the data from the fields in your data store.
+        //The data store could be a file or database or any other store based
+        //on your application.
+        console.log("wtf");
+        res.end("haha you got me");
+        // res.writeHead(200, {
+        //     'content-type': 'text/plain'
+        // });
+        // res.write('received the data:\n\n');
+        // res.end(util.inspect({
+        //     fields: fields,
+        //     files: files
+        // }));
+    });
+}
 module.exports = router;
