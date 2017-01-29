c01d50acbebebfa76c9235928db3afaf2034a526
gespiton <1044750905@qq.com>
2017-01-29 16:20:01 +0800

    uninstall package

--- routes/login.js
+++ routes/login.js
@@ -1,12 +1,9 @@
 var express = require('express');
 var router = express.Router();
-var formidable = require("express-formidable");
 var util = require("util");
 var http = require('http');
-var form = require('formidable');
 
 router.post('/', function (req, res, next) {
-    console.log('i am in');
     tryToSignIn(req, res);
 });
 
