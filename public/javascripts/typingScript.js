8f199a25d95fd1336eadf7246f6aa0443ad60e6e
gespiton <1044750905@qq.com>
2017-01-31 22:21:19 +0800

    refactoring & add backspace functionality

--- public/javascripts/typingScript.js
+++ public/javascripts/typingScript.js
@@ -1,54 +1,81 @@
 //noinspection JSUnresolvedFunction
 $(document).ready(function () {
-    let sampvarext = $('.passin').text();
-    console.log(sampvarext);
-    let textArray = sampvarext.split('');
+    let text = $('.passin').text();
+    let textArray = text.split('');
     let domArr = [];
     let curPos = 0;
     let unCorCount = 0;
 
-    // timer here
-    let start = new Date().getTime();
-    let started = false;
+    let startTime = new Date().getTime();
+    let typingStarted = false;
     let intervalID;
 
-    // fill the stage with char div
-    for (let char in textArray) {
-        let $charSpan = $('<span>', {
-            class: 'char',
-            text: textArray[char],
-        });
-        $('#stage').append($charSpan);
-        domArr.push($charSpan);
-    }
+    fillStage();
+    setFirstChar();
+    onKeyPressed();
+    backSpaceKeyHandler();
 
-    //set the first character as current character
-    $(domArr[0]).addClass('curChar');
     let length = domArr.length;
 
-    $(document).on("keypress",function (event) {
-        // prevent browser shotcut
-        event.preventDefault();
 
-        if (!started) {
-            intervalID = setInterval(updateWpf, 1000);
-            started = true;
-        }
+    function setFirstChar() {
+        $(domArr[0]).addClass('curChar');
+    }
 
-        check(String.fromCharCode(event.charCode));
+    function startWpfCal() {
+        intervalID = setInterval(updateWpf, 1000);
+        typingStarted = true;
+    }
+
+    function forwardCaret() {
         $(domArr[++curPos]).addClass('curChar');
         $(domArr[curPos - 1]).removeClass('curChar');
+    }
 
-        // when all the text is finished, turn off the keypress event
+    function backCaret() {
+        $(domArr[curPos]).removeClass('curChar correct incorrect fadeBgc');
+        $(domArr[--curPos]).removeClass('correct incorrect fadeBgc');
+        $(domArr[curPos].addClass('curChar'));
+
+    }
+
+    function isFinished() {
         if (curPos == length) {
             alert('finished');
             clearInterval(intervalID);
         }
-    });
+    }
+
+    function onKeyPressed() {
+        $(document).on("keypress", function (event) {
+            // prevent browser shotcut
+            // alert(event.hasOwnProperty());
+            event.preventDefault();
+
+            if (!typingStarted) {
+                startWpfCal();
+            }
+
+            check(String.fromCharCode(event.charCode));
+            forwardCaret();
+            isFinished();
+        });
+    }
+
+
+    function backSpaceKeyHandler() {
+        $(document).on("keydown", function (key) {
+            // alert(key.keyCode);
+            // 8
+            if (key.keyCode == 8) {
+                backCaret();
+            }
+        });
+    }
 
     function updateWpf() {
 
-        let elapsed = Math.floor((new Date().getTime() - start) / 100) / 10; // why not /1000
+        let elapsed = Math.floor((new Date().getTime() - startTime) / 100) / 10; // why not /1000
         // alert(elapsed);
         let wpf = Math.floor(((curPos + 1) / 5 - unCorCount) / (elapsed / 60));
         if (wpf < 0) {
@@ -58,13 +85,32 @@ $(document).ready(function () {
         $('#wpf').text(Math.floor((wpf)).toString());
     }
 
+    function fillStage() {
+        for (let char in textArray) {
+            let $charSpan = $('<span>', {
+                class: 'char',
+                text: textArray[char],
+            });
+            $('#stage').append($charSpan);
+            domArr.push($charSpan);
+        }
+    }
+
+    function correctChar() {
+        domArr[curPos].addClass('correct fadeBgc');
+    }
+
+    function wrongChar() {
+        ++unCorCount;
+        domArr[curPos].addClass('incorrect');
+    }
+
     function check(pressed) {
         // alert($(domArr[curPos]).text());
         if (pressed == $(domArr[curPos]).text()) {
-            domArr[curPos].addClass('correct fadeBgc');
+            correctChar();
         } else {
-            ++unCorCount;
-            domArr[curPos].addClass('incorrect');
+            wrongChar();
         }
     }
 });
