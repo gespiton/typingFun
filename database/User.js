75e3b3838928b78d6b094c6af5635d8e9ee01eb7
gespiton <1044750905@qq.com>
2017-04-17 11:52:13 +0800

    particle js mongod

--- database/User.js
+++ database/User.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://127.0.0.1:27017/myuser',function (err,db) {
+mongoose.createConnection('mongodb://127.0.0.1:27017/myuser',function (err,db) {
     if(err) throw err;
 });
 
